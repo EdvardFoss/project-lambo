8ba3d2ff4735f2ef441b00792bb8e70e4ca6cbf8
Eric Iro <eirikirvoll@mail.com>
2017-09-13 18:35:41 +0200

    Bigass update
    - Wrote new algorithm for createGame
    - General cleanup for variables

--- main.js
+++ main.js
@@ -8,7 +8,12 @@ var question_linkedTo = [];
 var question_multiplayer = [];
 var question_all = [];
 
-var current_game_questions = [];
+// JSON ARRAY CONTAING ID AND QUESTION
+var allQuestions = {};
+allQuestions.entries = new Array();
+
+var current_game_questions = {};
+current_game_questions.entries = new Array();
 
 // DELETED QUESTIONS
 var deleted_question_IDs = [];
@@ -46,51 +51,182 @@ function getQuestions(question_truth, question_dare, question_memory, question_l
                 // SPLIT UP IN ARRAYS BASED ON TYPE
 
                 var tall = allPlayers.length;
+                var j = 0;
+                var playerIndexThisCard;
+                var playerNameThisCard;
+                var newString;
+                var addedName="Balletissfaenskap";
 
                 for (var i = 0; i < obj.length; i++) {
 
-                  var playerIndexThisCard = Math.floor(Math.random()*tall);
-                  var playerNameThisCard = allPlayers[playerIndexThisCard];
-
                   if (obj[i].type === '1') {
                     // TRUTH
-                    if (obj[i].question.indexOf("PLAYER_NAME") > 0) {
+                    j = 0;
+                    addedName="koko";
+                    playerIndexThisCard = Math.floor(Math.random()*tall);
+                    playerNameThisCard = allPlayers[playerIndexThisCard];
 
-                      var newString = (obj[i].question).replace(/PLAYER_NAME/gi, 'TEST');
-                      alert(newString);
-                      question_truth.push(newString);
+                    while (j < 5) { // NOT MORE THAN 5 PLAYER_NAMES!!!!
+
+											 newString = (obj[i].question).replace("PLAYER_NAME", playerNameThisCard);
+
+                    	while(addedName==playerNameThisCard){
+                       			playerIndexThisCard = Math.floor(Math.random()*tall);
+                            playerNameThisCard = allPlayers[playerIndexThisCard];
+                      }
+
+                      obj[i].question = newString;
+                      j++;
+                      addedName=playerNameThisCard;
+
+                      playerIndexThisCard = Math.floor(Math.random()*tall);
+                    	playerNameThisCard = allPlayers[playerIndexThisCard];
                     }
-                    //str.replace(/xmas/i, 'Christmas');
 
-                    //console.log(question_truth);
+											allQuestions.entries.push({
+                       	"ID" : obj[i].linkedTo,
+                        "Type" : "1",
+                        "Question" : newString
+                      });
+                     //question_truth.push(newString);
+                     //console.log(question_truth);
+                     //console.log(allQuestions);
                   }
+
                     else if (obj[i].type === '2') {
-                      // DARE
-                      question_dare.push(obj[i].question);
+
+                      j = 0;
+                      addedName="koko";
+                      playerIndexThisCard = Math.floor(Math.random()*tall);
+                      playerNameThisCard = allPlayers[playerIndexThisCard];
+
+                      while (j < 5) { // NOT MORE THAN 5 PLAYER_NAMES!!!!
+
+                        addedName = playerNameThisCard;
+                        newString = (obj[i].question).replace("PLAYER_NAME", playerNameThisCard);
+
+                        while(addedName==playerNameThisCard){
+                         			playerIndexThisCard = Math.floor(Math.random()*tall);
+                              playerNameThisCard = allPlayers[playerIndexThisCard];
+                        }
+
+                        obj[i].question = newString;
+                        j++;
+                        addedName=playerNameThisCard;
+
+                        playerIndexThisCard = Math.floor(Math.random()*tall);
+                      	playerNameThisCard = allPlayers[playerIndexThisCard];
+                      }
+                        allQuestions.entries.push({
+                       	"ID" : obj[i].linkedTo,
+                        "Type" : "2",
+                        "Question" : newString
+                       });
+                       //question_dare.push(newString);
                       //console.log(question_dare);
+
                     }
 
                   else if (obj[i].type === '3') {
                     // MEMORY
-                    question_memory.push(obj[i].question);
+                    j = 0;
+                    addedName="koko";
+                    playerIndexThisCard = Math.floor(Math.random()*tall);
+                    playerNameThisCard = allPlayers[playerIndexThisCard];
+
+                    while (j < 5) { // NOT MORE THAN 5 PLAYER_NAMES!!!!
+
+                      addedName = playerNameThisCard;
+
+                      newString = (obj[i].question).replace("PLAYER_NAME", playerNameThisCard);
+
+                      while(addedName==playerNameThisCard){
+                       			playerIndexThisCard = Math.floor(Math.random()*tall);
+                            playerNameThisCard = allPlayers[playerIndexThisCard];
+                      }
+
+                      obj[i].question = newString;
+                      j++;
+                      addedName=playerNameThisCard;
+
+                      playerIndexThisCard = Math.floor(Math.random()*tall);
+                    	playerNameThisCard = allPlayers[playerIndexThisCard];
+                    }
+
+                     //question_memory.push(newString);
+                     allQuestions.entries.push({
+                     	"ID" : obj[i].linkedTo,
+                      "Type" : "3",
+                      "Question" : newString
+                     });
+
+                    //console.log(allQuestions);
                     //console.log(question_memory);
                   }
+
                   else if (obj[i].type === '4') {
                     // LINKED TO
-                    question_linkedTo.push(obj[i].question);
+                    allQuestions.entries.push({
+                     	"ID" : obj[i].linkedTo,
+                      "Type" : "4",
+                      "Question" : obj[i].question
+                     });
+                    //question_linkedTo.push(obj[i].question);
                     //console.log(question_linkedTo);
                   }
+
                   else if (obj[i].type === '5') {
                     // MULTIPLAYER
-                    question_multiplayer.push(obj[i].question);
+                    j = 0;
+                    addedName="koko";
+                    playerIndexThisCard = Math.floor(Math.random()*tall);
+                    playerNameThisCard = allPlayers[playerIndexThisCard];
+
+                    while (j < 5) { // NOT MORE THAN 5 PLAYER_NAMES!!!!
+
+                      addedName = playerNameThisCard;
+
+                      newString = (obj[i].question).replace("PLAYER_NAME", playerNameThisCard);
+                      addedName = playerNameThisCard;
+
+
+                      while(addedName==playerNameThisCard) {
+
+                       			playerIndexThisCard = Math.floor(Math.random()*tall);
+                            playerNameThisCard = allPlayers[playerIndexThisCard];
+                      }
+
+                      obj[i].question = newString;
+                      j++;
+                      addedName=playerNameThisCard;
+
+                      playerIndexThisCard = Math.floor(Math.random()*tall);
+                    	playerNameThisCard = allPlayers[playerIndexThisCard];
+                    }
+
+                    	allQuestions.entries.push({
+                     	"ID" : obj[i].linkedTo,
+                      "Type" : "5",
+                      "Question" : newString
+                     });
+
+                     //question_multiplayer.push(newString);
                     //console.log(question_multiplayer);
                   }
+
                   else if (obj[i].type === '6') {
                     // ALL
-                    question_all.push(obj[i].question);
+                    allQuestions.entries.push({
+                     	"ID" : obj[i].linkedTo,
+                      "Type" : "6",
+                      "Question" : obj[i].question
+                     });
+                    //question_all.push(obj[i].question);
                     //console.log(question_all);
                   }
+
                 }
+                //console.log(allQuestions.entries);
                 createGame(question_truth, question_dare, question_memory, question_linkedTo, question_multiplayer, question_all, truth_in, dare_in, memory_in, multiplayer_in, all_in);
             }
         };
@@ -98,54 +234,110 @@ function getQuestions(question_truth, question_dare, question_memory, question_l
         xmlhttp.send();
 
 }
-getQuestions(question_truth, question_dare, question_memory, question_linkedTo, question_multiplayer, question_all, truth_in, dare_in, memory_in, multiplayer_in, all_in);
+  getQuestions(question_truth, question_dare, question_memory, question_linkedTo, question_multiplayer, question_all, truth_in, dare_in, memory_in, multiplayer_in, all_in);
 
   function createGame(question_truth, question_dare, question_memory, question_linkedTo, question_multiplayer, question_all, truth_in, dare_in, memory_in, multiplayer_in, all_in) {
 
     var players = allPlayers;
-    var game_length = (players.length)*5;
-    //console.log(question_dare);
 
-    // ALGORITHM CALC NUMBERS
-    var truth_and_dare_count = (Math.round((game_length*2)/3));
-    var rest_of_questions = (game_length)-(truth_and_dare_count);
+    //-----------------------------------------
+    // randomizer
+    var start = 0;
+    var end = allQuestions.entries.length;
 
-    //SELECT QUESTIONS
-    var one_game_truth_and_dare_questions = [];
-    var one_game_rest_questions = [];
+    var randomNumber;
 
-    for (var i = 0; i < truth_and_dare_count; i++) {
+    var randomNumberThatHaveBeenUsed = new Array();
 
-        if (i % 2 == 0) {
-          //alert(question_truth[0]);
-          one_game_truth_and_dare_questions.push(question_truth[truth_in]);
-          current_game_questions.push(question_truth[truth_in]);
-          truth_in++;
-      } else {
-          one_game_truth_and_dare_questions.push(question_dare[dare_in]);
-          current_game_questions.push(question_dare[dare_in]);
-          dare_in++;
+    var game_length = (players.length)*5; // total number of questions
+
+    // aboslute numbers
+    var qTruth_count = Math.floor((game_length/100) * 30); // 30 %
+    var qDare_count = Math.floor((game_length/100) * 30); // 30 %
+    var qMemory_count = Math.floor((game_length/100) * 10); // 10 %
+    var qMultiplayer_count = Math.floor((game_length/100) * 15); // 15 %
+    var qAll_count = Math.floor((game_length/100) * 15); // 15 %
+
+    for (var i = 0; i < allQuestions.entries.length; i++) {
+
+      randomNumber = randomizeNumber(start, end);
+
+      if (allQuestions.entries[randomNumber].Type == '1') {
+
+        if (qTruth_count >= 0) { // push to array
+
+          current_game_questions.entries.push({
+            "ID" : allQuestions.entries[randomNumber].ID,
+            "Type" : "1",
+            "Question" : allQuestions.entries[randomNumber].Question
+          });
+
+          qTruth_count--;
+        }
+
+      } else if (allQuestions.entries[randomNumber].Type == '2') {
+
+        if (qDare_count >= 0) { // push to array
+
+          current_game_questions.entries.push({
+            "ID" : allQuestions.entries[randomNumber].ID,
+            "Type" : "2",
+            "Question" : allQuestions.entries[randomNumber].Question
+          });
+
+          qDare_count--;
+        }
+
+      } else if (allQuestions.entries[randomNumber].Type == '3') {
+
+        if (qMemory_count >= 0) { // push to array
+
+          current_game_questions.entries.push({
+            "ID" : allQuestions.entries[randomNumber].ID,
+            "Type" : "3",
+            "Question" : allQuestions.entries[randomNumber].Question
+          });
+
+          qMemory_count--;
+        }
+
+
+      } else if (allQuestions.entries[randomNumber].Type == '5') {
+
+        if (qMultiplayer_count >= 0) { // push to array
+
+          current_game_questions.entries.push({
+            "ID" : allQuestions.entries[randomNumber].ID,
+            "Type" : "5",
+            "Question" : allQuestions.entries[randomNumber].Question
+          });
+
+          qMultiplayer_count--;
+        }
+
+
+      } else if (allQuestions.entries[randomNumber].Type == '6') {
+
+        if (qAll_count >= 0) { // push to array
+
+          current_game_questions.entries.push({
+            "ID" : allQuestions.entries[randomNumber].ID,
+            "Type" : "6",
+            "Question" : allQuestions.entries[randomNumber].Question
+          });
+
+          qAll_count--;
+        }
       }
-    }
-
-    for (var i = 0; i < rest_of_questions; i++) {
-
-      if (i % 2 == 0) {
-        //alert(question_truth[0]);
-          one_game_rest_questions.push(question_multiplayer[multiplayer_in]);
-          current_game_questions.push(question_multiplayer[multiplayer_in]);
-          multiplayer_in++;
-      } else if (i % 3 == 0) {
-          one_game_rest_questions.push(question_all[all_in]);
-          current_game_questions.push(question_all[all_in]);
-          all_in++;
-      } else {
-        one_game_rest_questions.push(question_memory[memory_in]);
-        current_game_questions.push(question_memory[memory_in]);
-        memory_in++;
-      }
-
     }
     console.log(current_game_questions);
+
+  }
+
+  function randomizeNumber(start, end) {
+
+    var number = Math.floor(Math.random() * end) + start;
+
+    return number;
 
   }
